4d8b48637d6c8262a06d7cf28259d022aad332ef
charlesisfeline <62410083+charlesisfeline@users.noreply.github.com>
2026-07-24 13:44:44 -0400

    Update ChartEditorUploadVocalsDialog.hx

--- source/funkin/ui/debug/charting/dialogs/ChartEditorUploadVocalsDialog.hx
+++ source/funkin/ui/debug/charting/dialogs/ChartEditorUploadVocalsDialog.hx
@@ -244,7 +244,7 @@ class ChartEditorUploadVocalsDialog extends ChartEditorBaseDialog
 
     try
     {
-      var result:Null<Array<String>> = ChartEditorImportExportHandler.loadFromFNFCPath(chartEditorState, path.toString());
+      var result:Null<Array<String>> = ChartEditorImportExportHandler.loadSongFromFNFCPath(chartEditorState, path.toString());
       if (result != null)
       {
         chartEditorState.success('Loaded Chart',
@@ -273,7 +273,7 @@ class ChartEditorUploadVocalsDialog extends ChartEditorBaseDialog
     {
       try
       {
-        var result:Null<Array<String>> = ChartEditorImportExportHandler.loadFromFNFC(chartEditorState, selectedFile.bytes);
+        var result:Null<Array<String>> = ChartEditorImportExportHandler.loadSongFromFNFCPath(chartEditorState, selectedFile.bytes);
         if (result != null)
         {
           chartEditorState.success('Loaded Chart',
